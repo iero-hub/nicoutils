aabac3cdada99454036d559d13e8b25760d63b3c
iero-hub <iero-hub@gmail.com>
2020-05-10 20:43:50 +0900

    動作するようになった

--- src/content.js
+++ src/content.js
@@ -1,5 +1,19 @@
-'use strict'
+window.addEventListener("load", main, false);
 
-window.onload = function() {
-  document.getElementsByClassName('SeekBarHoverItem')[0].remove()
-};
+function main(e) {
+    const jsInitCheckTimer = setInterval(jsLoaded, 1000);
+    function jsLoaded() {
+      const balloon = 'SeekBarStoryboardPremiumLink-balloon-container'
+      const thumbnail = 'SeekBarStoryboardPremiumLink-thumbnail-container'
+      if (document.getElementsByClassName(balloon)[0] != null) {
+        clearInterval(jsInitCheckTimer);
+        //要素を取得する処理
+        document.getElementsByClassName(balloon)[0].remove()
+        document.getElementsByClassName(thumbnail)[0].remove()
+        console.log('kidousita')
+      };
+
+
+    }
+    console.log('deleted')
+}
